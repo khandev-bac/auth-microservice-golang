c3f9e1a749075a7c98bfdb674f63473788fdccbd
farhankhan <farhankhan@farhankhans-MacBook-Air.local>
2025-06-11 16:26:44 +0530

    added firebase verify

--- infrastructure/firebase/firebase.go
+++ infrastructure/firebase/firebase.go
@@ -1,11 +1,24 @@
 package firebase
 
-import "firebase.google.com/go/auth"
+import (
+	"context"
+
+	firebase "firebase.google.com/go"
+	"firebase.google.com/go/auth"
+	"google.golang.org/api/option"
+)
 
 type FirebaseService struct {
 	client *auth.Client
 }
 
-func NewFirebaseService(service_path string)*FirebaseService{
-	
+func NewFirebaseService(service_path string) *FirebaseService {
+	opt := option.WithCredentialsFile(service_path)
+	app, _ := firebase.NewApp(context.Background(), nil, opt)
+	client, _ := app.Auth(context.Background())
+	return &FirebaseService{client: client}
+}
+
+func (f *FirebaseService) VerifyByID(ctx context.Context, idToken string) (*auth.Token, error) {
+	return f.client.VerifyIDToken(ctx, idToken)
 }
