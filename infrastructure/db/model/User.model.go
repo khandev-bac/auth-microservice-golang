65ff67806bc9b1ce078bc036bacdcef125c01716
farhankhan <farhankhan@farhankhans-MacBook-Air.local>
2025-06-11 16:18:15 +0530

    added user model

---
+++ infrastructure/db/model/User.model.go
@@ -0,0 +1,21 @@
+package model
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID            uuid.UUID `gorm:"type:uuid;primaryKey"`
+	UserName      string    `gorm:"type:text;not null"`
+	UserEmail     string    `gorm:"type:text;unique;not null"`
+	Password      string    `gorm:"type:text;not null"`
+	UserProfile   string    `gorm:"type:text"`
+	FirebaseId    string    `gorm:"type:text"`
+	AuthProvider  string    `gorm:"type:text;default:password"`
+	EmailVerified bool      `gorm:"default:false"`
+	RefreshToken  string
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+}
