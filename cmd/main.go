fab05c0ca9ed764bc3cf82a60ad2e97beb1c3dc8
farhankhan <farhankhan@farhankhans-MacBook-Air.local>
2025-06-12 17:45:13 +0530

    updated main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,14 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
 
+	"github.com/khandev-bac/lemon/internals/router"
+)
 
+func main() {
+	router := router.MainRoute()
+	fmt.Println("Server started at port 3000")
+	http.ListenAndServe(":3000", router)
+}
